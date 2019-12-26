dd1eb6d1595a2621ac7a0adf8c4c394ecec89219
Andy <mingjing.zhou@orient.com>
2019-12-26 19:24:32 +0800

    函数 - this & 重载

--- section5/index.js
+++ section5/index.js
@@ -52,3 +52,92 @@ function buildNameRemain(firstName) {
 var buildNameFn = buildNameRemain;
 var result3 = buildNameFn('Bob', 'Adams', 'Will');
 console.log('buildNameRemain', result3);
+// this 是来自对象字面量的函数表达式
+// TypeScript 可以提示错误使用 this 的地方
+// 在 JavaScript 中会在函数调用时指定 this 的值, 但要弄清函数调用的上下文
+// 下面代码在编译时不会报错, 但在执行时会报 Cannot read property '2' of undefined
+// 原因是执行 cardPicker() 时调用的上下文是全局的 global, 此时 this 指向就是 global
+// 因此 this.suits 是 undefined
+// 使用 ES6 箭头函数语法可以解决, 原因是箭头函数保存的是函数创建时的 this 值
+var deck = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: Array(52),
+    createCardPicker: function () {
+        var _this = this;
+        // return function () {
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            // 随机的扑克牌索引
+            var pickedSuits = Math.floor(pickedCard / 13);
+            return {
+                // 通过索引得到扑克牌
+                suit: _this.suits[pickedSuits],
+                // 得到扑克牌的点数
+                card: pickedCard % 13
+            };
+        };
+    }
+};
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+console.log('card => ', pickedCard.card);
+var deckRefactor = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: Array(52),
+    // 把 this 显示的指向 Deck 接口
+    createCardPicker: function () {
+        var _this = this;
+        // return function () {
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            // 随机的扑克牌索引
+            var pickedSuits = Math.floor(pickedCard / 13);
+            return {
+                // 通过索引得到扑克牌
+                // 这样就不会出现 this.suits.s 而编译不报错了
+                suit: _this.suits[pickedSuits],
+                // 得到扑克牌的点数
+                card: pickedCard % 13
+            };
+        };
+    }
+};
+var cardPickerRefactor = deckRefactor.createCardPicker();
+var pickedCardRefactor = cardPickerRefactor();
+console.log('card refactor => ', pickedCardRefactor.card);
+var Handler = /** @class */ (function () {
+    function Handler() {
+        var _this = this;
+        this.onClickBad = function (e) {
+            _this.type = e.type;
+        };
+    }
+    return Handler;
+}());
+var h = new Handler();
+var uiElement = {
+    addClickListener: function () {
+    }
+};
+uiElement.addClickListener(h.onClickBad);
+// 函数重载
+var suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+function pickCard(x) {
+    if (Array.isArray(x)) {
+        var pickedCard_1 = Math.floor(Math.random() * x.length);
+        return pickedCard_1;
+    }
+    else if (typeof x === 'number') {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+var myDeck = [
+    { suit: 'diamonds', card: 2 },
+    { suit: 'spades', card: 10 },
+    { suit: 'hearts', card: 4 }
+];
+var pickedCard1 = myDeck[pickCard(myDeck)];
+console.log('card: ' + pickedCard1.card + ' of ' + pickedCard1.suit);
+var pickedCard2 = pickCard(15);
+console.log('card: ' + pickedCard2.card + ' of ' + pickedCard2.suit);
--- section5/index.ts
+++ section5/index.ts
@@ -50,3 +50,115 @@ function buildNameRemain(firstName: string, ...restOfName: string[]): string {
 let buildNameFn: (fname: string, ...rest: string[]) => string = buildNameRemain
 let result3 = buildNameFn('Bob', 'Adams', 'Will')
 console.log('buildNameRemain', result3)
+
+// this 是来自对象字面量的函数表达式
+// TypeScript 可以提示错误使用 this 的地方
+// 在 JavaScript 中会在函数调用时指定 this 的值, 但要弄清函数调用的上下文
+// 下面代码在编译时不会报错, 但在执行时会报 Cannot read property '2' of undefined
+// 原因是执行 cardPicker() 时调用的上下文是全局的 global, 此时 this 指向就是 global
+// 因此 this.suits 是 undefined
+// 使用 ES6 箭头函数语法可以解决, 原因是箭头函数保存的是函数创建时的 this 值
+let deck = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: Array(52),
+    createCardPicker: function () {
+        // return function () {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52)
+            // 随机的扑克牌索引
+            let pickedSuits = Math.floor(pickedCard / 13)
+
+            return {
+                // 通过索引得到扑克牌
+                suit: this.suits[pickedSuits],
+                // 得到扑克牌的点数
+                card: pickedCard % 13
+            }
+        }
+    }
+}
+let cardPicker = deck.createCardPicker()
+let pickedCard = cardPicker()
+console.log('card => ', pickedCard.card)
+
+// 使用 function (this: void) 重构 deck
+interface Card {
+    suit: string
+    card: number
+}
+interface Deck {
+    suits: string[]
+    cards: number[]
+
+    createCardPicker(this: Deck): () => Card
+}
+let deckRefactor: Deck = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: Array(52),
+    // 把 this 显示的指向 Deck 接口
+    createCardPicker: function (this: Deck) {
+        // return function () {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52)
+            // 随机的扑克牌索引
+            let pickedSuits = Math.floor(pickedCard / 13)
+
+            return {
+                // 通过索引得到扑克牌
+                // 这样就不会出现 this.suits.s 而编译不报错了
+                suit: this.suits[pickedSuits],
+                // 得到扑克牌的点数
+                card: pickedCard % 13
+            }
+        }
+    }
+}
+let cardPickerRefactor = deckRefactor.createCardPicker()
+let pickedCardRefactor = cardPickerRefactor()
+console.log('card refactor => ', pickedCardRefactor.card)
+
+// this 参数在回调函数中
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void ): void
+}
+class Handler {
+    type: string
+
+    onClickBad = (e: Event) => {
+        this.type = e.type
+    }
+}
+let h = new Handler()
+let uiElement: UIElement = {
+    addClickListener() {
+    }
+}
+uiElement.addClickListener(h.onClickBad)
+
+// 函数重载
+let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+
+// 使用函数重载来做约束输入的参数类型条件
+function pickCard(x: { suit: string, card: number }[]): number
+function pickCard(x: number): { suit: string, card: number }
+
+function pickCard(x): any {
+    if (Array.isArray(x)) {
+        let pickedCard = Math.floor(Math.random() * x.length)
+        return pickedCard
+    } else if (typeof x === 'number') {
+        let pickedSuit = Math.floor(x / 13)
+        return { suit: suits[pickedSuit], card: x % 13 }
+    }
+}
+
+let myDeck = [
+    { suit: 'diamonds', card: 2},
+    { suit: 'spades', card: 10},
+    { suit: 'hearts', card: 4}
+]
+let pickedCard1 = myDeck[pickCard(myDeck)]
+console.log('card: ' + pickedCard1.card + ' of ' + pickedCard1.suit)
+
+let pickedCard2 = pickCard(15)
+console.log('card: ' + pickedCard2.card + ' of ' + pickedCard2.suit)
