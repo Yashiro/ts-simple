cb932803e773c0a3d103116add534f3f851added
Andy <mingjing.zhou@orient.com>
2019-12-31 14:59:41 +0800

    高级类型 - 类型保护
    Signed-off-by: Andy <mingjing.zhou@orient.com>

--- section7/index.js
+++ section7/index.js
@@ -59,4 +59,81 @@ function getSmallPet() {
 var pet = getSmallPet();
 // layEggs 是 Fish 和 Bird 共有的函数可以被 pet 调用
 // 而其他独有的函数则不能被 pet 调用
-pet.layEggs();
+// pet.layEggs()
+// 类型保护
+// 如果没有类型保护, 一般通过类型断言来明确类型
+// let petTypeProtect = getSmallPet()
+// if ((petTypeProtect as Fish).swim) {
+//     (petTypeProtect as Fish).swim()
+// } else if ((petTypeProtect as Bird).fly) {
+//     (petTypeProtect as Bird).fly()
+// }
+// 类型谓词
+// function isFish(petTypeProtect: Fish | Bird): petTypeProtect is Fish {
+//     return (petTypeProtect as Fish).swim !== undefined
+// }
+// if (isFish(petTypeProtect)) {
+//     petTypeProtect.swim()
+// } else {
+//     petTypeProtect.fly()
+// }
+// 使用类型谓词改写 padLeft 函数
+function isNumber(x) {
+    return typeof x === 'number';
+}
+function isString(x) {
+    return typeof x === 'string';
+}
+// function padLeftTypeProtect(value: string, padding: string | number) {
+//     if (isNumber(padding)) {
+//         return Array(padding + 1).join('  ') + value
+//     }
+//     if (isString(padding)) {
+//         return padding + value
+//     }
+//     throw new Error(`Expected string or number got ${padding}`)
+// }
+// 也可使用 type 来判定参数类型来进行类型保, 但只对基础类型有效
+function padLeftTypeProtect(value, padding) {
+    if (typeof padding === 'number') {
+        return Array(padding + 1).join('  ') + value;
+    }
+    if (typeof padding === 'string') {
+        return padding + value;
+    }
+    throw new Error("Expected string or number got " + padding);
+}
+console.log('padLeftTypeProtect => ', padLeftTypeProtect('Hello World', 4));
+// instance 类型保护
+var Tiger = /** @class */ (function () {
+    function Tiger() {
+    }
+    Tiger.prototype.roar = function () {
+        console.log('tiger roar');
+    };
+    Tiger.prototype.sleep = function () {
+        console.log('tiger sleep');
+    };
+    return Tiger;
+}());
+var Sheep = /** @class */ (function () {
+    function Sheep() {
+    }
+    Sheep.prototype.bleat = function () {
+        console.log('sheep bleat');
+    };
+    Sheep.prototype.sleep = function () {
+        console.log('sheep  sleep');
+    };
+    return Sheep;
+}());
+function getRandomPet() {
+    return Math.random() > 0.5 ? new Tiger() : new Sheep();
+}
+var petInstanceProtect = getRandomPet();
+if (petInstanceProtect instanceof Tiger) {
+    petInstanceProtect.roar();
+}
+if (petInstanceProtect instanceof Sheep) {
+    petInstanceProtect.bleat();
+}
--- section7/index.ts
+++ section7/index.ts
@@ -3,7 +3,6 @@
 // 可以把现有的多种类型叠加在一起成为新的类型
 // 多数是在混入或其他不适合典型面向对象模型的地方下使用
 // 交叉类型由两个泛型和 & 连接而成
-
 function extend<T, U>(first: T, second: U): T & U {
     // 此处断言为 T & U 类型
     let result = {} as T & U
@@ -77,3 +76,78 @@ let pet = getSmallPet()
 // layEggs 是 Fish 和 Bird 共有的函数可以被 pet 调用
 // 而其他独有的函数则不能被 pet 调用
 pet.layEggs()
+
+// 类型保护
+// 如果没有类型保护, 一般通过类型断言来明确类型
+let petTypeProtect = getSmallPet()
+if ((petTypeProtect as Fish).swim) {
+    (petTypeProtect as Fish).swim()
+} else if ((petTypeProtect as Bird).fly) {
+    (petTypeProtect as Bird).fly()
+}
+// 类型谓词
+function isFish(petTypeProtect: Fish | Bird): petTypeProtect is Fish {
+    return (petTypeProtect as Fish).swim !== undefined
+}
+if (isFish(petTypeProtect)) {
+    petTypeProtect.swim()
+} else {
+    petTypeProtect.fly()
+}
+// 使用类型谓词改写 padLeft 函数
+function isNumber(x: any): x is number {
+    return typeof x === 'number'
+}
+function isString(x: any): x is string {
+    return typeof x === 'string'
+}
+// function padLeftTypeProtect(value: string, padding: string | number) {
+//     if (isNumber(padding)) {
+//         return Array(padding + 1).join('  ') + value
+//     }
+//     if (isString(padding)) {
+//         return padding + value
+//     }
+//     throw new Error(`Expected string or number got ${padding}`)
+// }
+// 也可使用 type 来判定参数类型来进行类型保, 但只对基础类型有效
+function padLeftTypeProtect(value: string, padding: string | number) {
+    if (typeof padding === 'number') {
+        return Array(padding + 1).join('  ') + value
+    }
+    if (typeof padding === 'string') {
+        return padding + value
+    }
+    throw new Error(`Expected string or number got ${padding}`)
+}
+console.log('padLeftTypeProtect => ', padLeftTypeProtect('Hello World', 4))
+
+// instance 类型保护
+class Tiger {
+    roar() {
+        console.log('tiger roar')
+    }
+
+    sleep() {
+        console.log('tiger sleep')
+    }
+}
+class Sheep {
+    bleat() {
+        console.log('sheep bleat')
+    }
+
+    sleep() {
+        console.log('sheep  sleep')
+    }
+}
+function getRandomPet(): Tiger | Sheep {
+    return Math.random() > 0.5 ? new Tiger() : new Sheep()
+}
+let petInstanceProtect = getRandomPet()
+if (petInstanceProtect instanceof Tiger) {
+    petInstanceProtect.roar()
+}
+if (petInstanceProtect instanceof Sheep) {
+    petInstanceProtect.bleat()
+}
